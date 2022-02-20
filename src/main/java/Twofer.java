2b5e4648e39ad4844746a73bbde0d5ae8885d133
Jlee108 <jbacked@yahoo.com>
2022-02-20 18:33:48 +0000

    Refactoring

--- src/main/java/Twofer.java
+++ src/main/java/Twofer.java
@@ -1,12 +1,7 @@
+import static java.util.Optional.ofNullable;
+
 public class Twofer {
     public String twofer(String name) {
-        if (name == null) {
-            return "One for you, one for me.";
-        } else if (name == "Alice") {
-            return "One for Alice, one for me.";
-        } else if (name == "Bob") {
-            return "One for Bob, one for me.";
-        } else
-            return "One for , one for me.";
+        return String.format("One for %s, one for me.", ofNullable(name).orElse("you"));
     }
 }
